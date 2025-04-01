cf07d1454fb07f39234ef0aa9aaa6a7a30f8ac8c
Deeja-ish <khadijaismail696@gmail.com>
2025-04-01 21:46:17 +0100

    Completed

--- src/index.js
+++ src/index.js
@@ -11,18 +11,22 @@ function displayStory(response){
 function generateStory(event){
     event.preventDefault();
 
-    let input = document.querySelector("#story-input")
+    let userInput = document.querySelector("#story-input");
     
-    let apiKey = "a27d320o50dd0fb63e6t6dc778bc438b"
-    let prompt = `Generate me an amazing ${input}`
-    let context = "Please make the story very short and easy to understand for both adults and children and use emojis and make the stories about humans. please stop using Null"
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    let apiKey = "a27d320o50dd0fb63e6t6dc778bc438b";
+    let prompt = `Generate me an amazing and incredible ${userInput.value}`;
+    let context = "Please make the story very short and easy to understand for both adults and children and use emojis and make the stories about humans. please stop using Null. use different characters each time and different scenarios";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let hidden = document.querySelector(".hidden");
+    hidden.classList.remove("hidden");
+    hidden.innerHTML = `<span class= "blink">⌛</span>Generating a ${userInput.value}`;
 
     console.log("Generate Story")
     console.log(`Prompt: ${prompt}`)
     console.log(`Context: ${context}`)
 
-    axios.get(apiUrl).then(displayStory)
+    axios.get(apiUrl).then(displayStory);
 }
 
 let storyGenerator = document.querySelector("#story-generator");
